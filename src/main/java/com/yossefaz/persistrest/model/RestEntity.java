8f7f8ec4afb3117b1c013356ccc355875183b0a3
yossefazoulay <azoulayos@protonmail.com>
2020-06-16 16:33:21 +0300

    Full support for PostgreSQL Json Data type

--- src/main/java/com/yossefaz/persistrest/model/RestEntity.java
+++ src/main/java/com/yossefaz/persistrest/model/RestEntity.java
@@ -1,9 +1,14 @@
 package com.yossefaz.persistrest.model;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
 import java.util.UUID;
@@ -11,7 +16,12 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Data
 @Table(name = "persistent_rest")
+@TypeDef(
+        name = "jsonb-node",
+        typeClass = JsonNodeBinaryType.class
+)
 public class RestEntity {
 
     @Id
@@ -23,10 +33,12 @@ public class RestEntity {
     @Column(name = "url")
     private String url;
 
+    @Type(type = "jsonb-node")
     @Column(name = "payload", columnDefinition = "json")
-    private String payload;
+    private JsonNode payload;
 
+    @Type(type = "jsonb-node")
     @Column(name = "headers", columnDefinition = "json")
-    private String headers;
+    private JsonNode headers;
 
 }
